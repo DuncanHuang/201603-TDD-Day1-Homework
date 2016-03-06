943117b5cb0955d7d0cde1d0fb2d3e745f64638e
Duncan <th710703@gmail.com>
2016-03-06 20:18:49 +0800

    讓 data 可以由使用者端傳入，測試回傳集合數直接指定

--- app/sellAnalysis.php
+++ app/sellAnalysis.php
@@ -10,33 +10,8 @@ namespace homework;
 
 class sellAnalysis
 {
-    private function getData()
+    public function analysis($data, $rowsGroup, $groupName)
     {
-        $data = array(
-            array('Id' => 1, 'Cost' => 1, 'Revenue' => 11, 'SellPrice' => 21),
-            array('Id' => 2, 'Cost' => 2, 'Revenue' => 12, 'SellPrice' => 22),
-            array('Id' => 3, 'Cost' => 3, 'Revenue' => 13, 'SellPrice' => 23),
-            array('Id' => 4, 'Cost' => 4, 'Revenue' => 14, 'SellPrice' => 24),
-            array('Id' => 5, 'Cost' => 5, 'Revenue' => 15, 'SellPrice' => 25),
-            array('Id' => 6, 'Cost' => 6, 'Revenue' => 16, 'SellPrice' => 26),
-            array('Id' => 7, 'Cost' => 7, 'Revenue' => 17, 'SellPrice' => 27),
-            array('Id' => 8, 'Cost' => 8, 'Revenue' => 18, 'SellPrice' => 28),
-            array('Id' => 9, 'Cost' => 9, 'Revenue' => 19, 'SellPrice' => 29),
-            array('Id' => 10, 'Cost' => 10, 'Revenue' => 20, 'SellPrice' => 30),
-            array('Id' => 11, 'Cost' => 11, 'Revenue' => 21, 'SellPrice' => 31),
-        );
-        return $data;
-    }
-
-    public function getRows()
-    {
-        return count($this->getData());
-    }
-
-    public function analysis($rowsGroup, $groupName)
-    {
-        $data = $this->getData();
-
         $chunk = array_chunk($data, $rowsGroup);
 
         $sum = array();
--- tests/sellAnalysisTest.php
+++ tests/sellAnalysisTest.php
@@ -6,9 +6,27 @@
  * Date: 2016/3/5
  * Time: 下午9:32
  */
-
 class sellAnalysisTest extends PHPUnit_Framework_TestCase
 {
+    private function getData()
+    {
+        $data = array(
+            array('Id' => 1, 'Cost' => 1, 'Revenue' => 11, 'SellPrice' => 21),
+            array('Id' => 2, 'Cost' => 2, 'Revenue' => 12, 'SellPrice' => 22),
+            array('Id' => 3, 'Cost' => 3, 'Revenue' => 13, 'SellPrice' => 23),
+            array('Id' => 4, 'Cost' => 4, 'Revenue' => 14, 'SellPrice' => 24),
+            array('Id' => 5, 'Cost' => 5, 'Revenue' => 15, 'SellPrice' => 25),
+            array('Id' => 6, 'Cost' => 6, 'Revenue' => 16, 'SellPrice' => 26),
+            array('Id' => 7, 'Cost' => 7, 'Revenue' => 17, 'SellPrice' => 27),
+            array('Id' => 8, 'Cost' => 8, 'Revenue' => 18, 'SellPrice' => 28),
+            array('Id' => 9, 'Cost' => 9, 'Revenue' => 19, 'SellPrice' => 29),
+            array('Id' => 10, 'Cost' => 10, 'Revenue' => 20, 'SellPrice' => 30),
+            array('Id' => 11, 'Cost' => 11, 'Revenue' => 21, 'SellPrice' => 31),
+        );
+
+        return $data;
+    }
+
     public function test_threeRowsGroup_cal_sumCost()
     {
         // Arrange
@@ -17,10 +35,11 @@ class sellAnalysisTest extends PHPUnit_Framework_TestCase
         // Act
         $rowsGroup = 3;
         $groupName = 'Cost';
-        $target    = $analysis->analysis($rowsGroup, $groupName);
+        $groupNum  = 4;
+        $target    = $analysis->analysis($this->getData(), $rowsGroup, $groupName);
 
         // Assert
-        $this->assertCount((int) ceil($analysis->getRows() / $rowsGroup), $target);
+        $this->assertCount($groupNum, $target);
         $this->assertEquals(6, $target[0]);
         $this->assertEquals(15, $target[1]);
         $this->assertEquals(24, $target[2]);
@@ -35,10 +54,11 @@ class sellAnalysisTest extends PHPUnit_Framework_TestCase
         // Act
         $rowsGroup = 4;
         $groupName = 'Revenue';
-        $target    = $analysis->analysis($rowsGroup, $groupName);
+        $groupNum  = 3;
+        $target    = $analysis->analysis($this->getData(), $rowsGroup, $groupName);
 
         // Assert
-        $this->assertCount((int) ceil($analysis->getRows() / $rowsGroup), $target);
+        $this->assertCount($groupNum, $target);
         $this->assertEquals(50, $target[0]);
         $this->assertEquals(66, $target[1]);
         $this->assertEquals(60, $target[2]);
